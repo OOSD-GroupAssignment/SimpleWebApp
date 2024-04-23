d9f6658be6bfaffd3432f17ddc5ddf248e6ceb8f
Tonyrine <157473803+Tonyrine@users.noreply.github.com>
2024-04-23 04:07:27 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 define('DB_HOST','localhost:3306');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
-define('DB_DATABASE','21050513026');
+define('DB_DATABASE','datadata');
 $conn = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die ("Unable to connet");
 	
 
